ebaa27bdcb4d954635960442bbfa39830a38b294
Pand01ey <92025288+Pand01ey@users.noreply.github.com>
2023-10-05 12:01:29 +0530

    Add files via upload

---
+++ Medical.py
@@ -0,0 +1,44 @@
+import numpy as np
+import joblib
+import streamlit as st
+import sklearn
+import pickle
+
+# loading the saved model
+loaded_model = pickle.load('Medical.pkl')
+
+
+
+#creating a function for Prediction
+def medical_insurance_cost_prediction(input_data):
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    return prediction
+
+#giving a title
+st.title('Medical Insurance Cost Prediction')
+
+#getting input from the user
+
+age = st.text_input('Age')
+sex = st.text_input('Sex: 0 -> Female, 1 -> Male')
+bmi = st.text_input('Body Mass Index')
+children = st.text_input('Number of Children')
+smoker = st.text_input('Smoker: 0 -> No, 1 -> Yes')
+region = st.text_input('Region of Living: 0 -> NorthEast, 1-> NorthWest, 2-> SouthEast, 3-> SouthWest')
+
+#code for prediction
+diagnosis = ''
+
+# getting the input data from the user
+if st.button('Predicted Medical Insurance Cost: '):
+    diagnosis = medical_insurance_cost_prediction([age,sex,bmi,children,smoker,region])
+    
+st.success(diagnosis)
